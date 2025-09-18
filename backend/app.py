1b15add4624c09751f0ea6b80859afb0621b6454
Thilak Divyadharshan <thilakdivyadharshan@karunya.edu.in>
2025-09-19 00:35:36 +0530

    Added CI/CD workflow, updated backend/app.py, and Kubernetes manifests

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return {"message": "Hello from the backend!"}
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app = Flask(name)
+@app.route('/greet')
+def greet():
+return 'Hello from Backend Microservice!'
+if name == 'main':
+app.run(host='0.0.0.0', port=5000)
